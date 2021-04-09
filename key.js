5763c0e1854d11d2bc83f2662b622557df636ac4
unknown <jackyjohn1206@gmail.com>
2021-04-09 16:35:00 +0800

    first commit

--- key.js
+++ key.js
@@ -1,8 +1,8 @@
 //API KEY https://developers.google.com/custom-search/v1/using_rest
-export const API_KEY = "AIzaSyBCN-VgWd3ougJ_QYS2kG3sKZyQcPtyKo4";
+export const API_KEY = "API KEYS from Google";
 
 //Context Key https://cse.google.com/cse/create/new
-export const CONTEXT_KEY = "3303814c63170efd9";
+export const CONTEXT_KEY = "Context KEYS from Google";
 
 //<script async src="https://cse.google.com/cse.js?cx=3303814c63170efd9"></script>
 //<div class="gcse-search"></div>
