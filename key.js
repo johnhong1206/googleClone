da9ca307f6adb05ecf679f9249c02905a7da9107
unknown <jackyjohn1206@gmail.com>
2021-04-09 16:37:39 +0800

    first commit

--- key.js
+++ key.js
@@ -1,8 +1,6 @@
 //API KEY https://developers.google.com/custom-search/v1/using_rest
-export const API_KEY = "API KEYS from Google";
+export const API_KEY =
+  "API KEY should request from Google with 100 limit usage so this project is using dummy data after first fetch";
 
 //Context Key https://cse.google.com/cse/create/new
 export const CONTEXT_KEY = "Context KEYS from Google";
-
-//<script async src="https://cse.google.com/cse.js?cx=3303814c63170efd9"></script>
-//<div class="gcse-search"></div>
